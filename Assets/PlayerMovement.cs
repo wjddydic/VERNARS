89b7dcacd8be29d216e001e62fdafa4f7ca09707
wjddydic <31722525+wjddydic@users.noreply.github.com>
2017-12-02 10:26:33 +0900

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float maxSpeed = 5f;
+    public float rotSpeed = 189f;
+
+    float shipBoundaryRadius = 0.5f;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        // 우주선 회전 (A,D)
+        Quaternion rot = transform.rotation;
+        float z = rot.eulerAngles.z;
+        z -= Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        rot = Quaternion.Euler( 0, 0, z);
+        transform.rotation = rot;
+
+
+        // 우주선 이동 (W,S)
+        Vector3 pos = transform.position;
+
+        Vector3 velocity = new Vector3 (0, Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime, 0);
+
+        pos += rot * velocity;
+
+        // 카메라 밖으로 안나가게
+        if(pos.y + shipBoundaryRadius > Camera.main.orthographicSize)
+        {
+            pos.y = Camera.main.orthographicSize - shipBoundaryRadius;
+        }
+
+        if (pos.y - shipBoundaryRadius < -Camera.main.orthographicSize)
+        {
+            pos.y = -Camera.main.orthographicSize + shipBoundaryRadius;
+        }
+
+
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float widthOrtho = Camera.main.orthographicSize * screenRatio;
+
+        if (pos.x + shipBoundaryRadius > widthOrtho)
+        {
+            pos.x = widthOrtho - shipBoundaryRadius;
+        }
+
+        if (pos.x - shipBoundaryRadius < -widthOrtho)
+        {
+            pos.x = -widthOrtho + shipBoundaryRadius;
+        }
+
+
+
+        transform.position = pos;
+
+
+	}
+}
